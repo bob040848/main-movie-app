9cb422884ebb086f8d7b78359ab2f4c1c0faddb4
25LP3117 25LP3117 <obilguudei48@gmai.com>
2025-05-07 12:06:59 +0800

    2

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,12 @@
 import SearchPage from "@/components/common/SeachPage";
+import { Suspense } from "react";
 
 const Search = () => {
-  return <SearchPage />;
+  return (
+    <Suspense>
+      <SearchPage />;
+    </Suspense>
+  );
 };
 
 export default Search;
